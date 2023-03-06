32990c2e5c583ee4030cbc598a0b6f4f5e58364e
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-06 20:07:43 -0300

    Comitzin de leve

---
+++ format.py
@@ -0,0 +1,8 @@
+a = "Matheus"
+b = 23
+c = "anos"
+
+texto = "O meu nome {nome}"
+
+exibir = texto.format(nome=a)
+print(exibir)
--- operador.py
+++ operador.py
@@ -1,9 +1,13 @@
-adicao = 2+2
-subtracao = 2-2
-multiplicacao =2*2
-divisao = 10/2
+# adicao = 2+2
+# subtracao = 2-2
+# multiplicacao =2*2
+# divisao = 10//2
+# modulo = 10 % 4
 
-print(adicao)
-print(subtracao)
-print("A multiplicação é: ",multiplicacao)
-print("A divisão é: ",divisao)
+# print(adicao)
+# print(subtracao)
+# print("A multiplicação é: ",multiplicacao)
+# print("A divisão é: ",divisao)
+# print("O modulo é: ",modulo)
+unir = "Matheus "+ "Qualidade " * 5
+print(unir)

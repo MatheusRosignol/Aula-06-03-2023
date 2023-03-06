61a78b9b6be2c9652ac674f4f4b5e5707289a335
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-06 20:40:01 -0300

    Commitzin de qualidade

---
+++ condicao.py
@@ -0,0 +1,6 @@
+pergunta = input("Qual a sua cidade de nascimento?: ")
+
+if pergunta == "São Paulo":
+    print("Você é paulistano")
+else:
+    print("Você é tilanbucano")
--- format.py
+++ format.py
@@ -1,8 +1,17 @@
 a = "Matheus"
 b = 23
 c = "anos"
+d = "de idade"
+# texto = "O meu nome {nome}"
 
-texto = "O meu nome {nome}"
+# exibir = texto.format(nome=a)
+# print(exibir)
 
-exibir = texto.format(nome=a)
+texto2= "Tenho {0} {1} {2}"
+
+exibir = texto2.format(b,c,d)
+
 print(exibir)
+
+nome = input("Qual o seu nome:? ")
+print("Seu nome é ",nome)

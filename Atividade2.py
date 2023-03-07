2f2d5b9c7ab68ae5ca58ada2b1d9de2f1189ddcc
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-06 21:22:13 -0300

    Atividade 2 pronta

---
+++ Atividade2.py
@@ -0,0 +1,8 @@
+Prim_numero = input("Digite o primeiro número: ")
+Sec_numero = input("Digite o segundo número: ")
+if Prim_numero > Sec_numero:
+    print("O maior número é o: ",Prim_numero)
+else:
+    print("O maior número é o: ",Sec_numero)
+if Prim_numero == Sec_numero:
+    print("Não há maior número")     
